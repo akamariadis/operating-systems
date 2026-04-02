d8ef13177147afdbf7227618d21d10f233d2190c
Angelos Kamariadis <150456060+akamariadis@users.noreply.github.com>
2026-04-02 17:17:26 +0300

    Update part4_main.c

--- Lab 1 Code/part4_main.c
+++ Lab 1 Code/part4_main.c
@@ -38,19 +38,15 @@ void set_nonblocking(int fd) {
 void run_dispatcher(const char *filename, int pipe_fe_to_disp[2], int pipe_disp_to_fe[2], pid_t fe_pid) {
     close(pipe_fe_to_disp[1]);
     close(pipe_disp_to_fe[0]);
-
     set_nonblocking(pipe_fe_to_disp[0]);
-
     WorkerInfo workers[MAX_WORKERS];
     for (int i = 0; i < MAX_WORKERS; i++) workers[i].active = 0;
-
     ChunkInfo chunks[MAX_CHUNKS];
     int total_chunks = 0;
     int chunks_completed = 0;
     int total_found = 0;
     int search_in_progress = 0;
     char current_target = '\0';
-
     int pipe_work_to_disp[2];
     pipe(pipe_work_to_disp);
     set_nonblocking(pipe_work_to_disp[0]);
@@ -71,7 +67,6 @@ void run_dispatcher(const char *filename, int pipe_fe_to_disp[2], int pipe_disp_
                 if (workers[i].active && workers[i].pid == dead_pid) {
                     workers[i].active = 0;
                     close(workers[i].pipe_to_worker[1]);
-
                     int c_id = workers[i].assigned_chunk;
                     if (c_id != -1 && chunks[c_id].status == 1) {
                         chunks[c_id].status = 0;
@@ -80,7 +75,6 @@ void run_dispatcher(const char *filename, int pipe_fe_to_disp[2], int pipe_disp_
                 }
             }
         }
-
         if (status_requested) {
             status_requested = 0;
             char reply[128];
@@ -93,7 +87,6 @@ void run_dispatcher(const char *filename, int pipe_fe_to_disp[2], int pipe_disp_
             write(pipe_disp_to_fe[1], reply, strlen(reply) + 1);
             kill(fe_pid, SIGCONT);
         }
-
         if (read(pipe_fe_to_disp[0], msg_buf, sizeof(msg_buf)) > 0) {
             if (strncmp(msg_buf, "ADD", 3) == 0) {
                 int added = 0;
@@ -104,12 +97,10 @@ void run_dispatcher(const char *filename, int pipe_fe_to_disp[2], int pipe_disp_
                         if (w_pid == 0) {
                             close(pipe_fe_to_disp[0]); close(pipe_disp_to_fe[1]);
                             close(pipe_work_to_disp[0]); close(workers[i].pipe_to_worker[1]);
-
                             char id_str[10], in_str[10], out_str[10];
                             sprintf(id_str, "%d", i);
                             sprintf(in_str, "%d", workers[i].pipe_to_worker[0]);
                             sprintf(out_str, "%d", pipe_work_to_disp[1]);
-
                             char *args[] = {"./worker", id_str, in_str, out_str, (char *)filename, NULL};
                             execv("./worker", args);
                             exit(1);
@@ -141,7 +132,6 @@ void run_dispatcher(const char *filename, int pipe_fe_to_disp[2], int pipe_disp_
                 chunks_completed = 0;
                 total_found = 0;
                 search_in_progress = 1;
-
                 char resp[] = "Search started in background.\n";
                 write(pipe_disp_to_fe[1], resp, strlen(resp) + 1);
             }
@@ -157,7 +147,6 @@ void run_dispatcher(const char *filename, int pipe_fe_to_disp[2], int pipe_disp_
                         if (workers[w].active && workers[w].assigned_chunk == -1) {
                             workers[w].assigned_chunk = c;
                             chunks[c].status = 1;
-
                             char task_msg[32];
                             sprintf(task_msg, "TSK %08ld %08ld %c", chunks[c].offset, chunks[c].size, current_target);
                             write(workers[w].pipe_to_worker[1], task_msg, 32);
@@ -167,17 +156,14 @@ void run_dispatcher(const char *filename, int pipe_fe_to_disp[2], int pipe_disp_
                 }
             }
         }
-
         char res_msg[16];
         if (read(pipe_work_to_disp[0], res_msg, 16) == 16) {
             int w_id, found;
             sscanf(res_msg, "RES %d %d", &w_id, &found);
-
             total_found += found;
             chunks[workers[w_id].assigned_chunk].status = 2;
             workers[w_id].assigned_chunk = -1;
             chunks_completed++;
-
             if (search_in_progress && chunks_completed == total_chunks) {
                 search_in_progress = 0;
                 char final_msg[128];
@@ -185,7 +171,6 @@ void run_dispatcher(const char *filename, int pipe_fe_to_disp[2], int pipe_disp_
                 write(pipe_disp_to_fe[1], final_msg, strlen(final_msg) + 1);
             }
         }
-
         usleep(10000);
     }
 }
@@ -194,11 +179,10 @@ void run_frontend(int pipe_fe_to_disp[2], int pipe_disp_to_fe[2], pid_t dispatch
     close(pipe_fe_to_disp[0]);
     close(pipe_disp_to_fe[1]);
     fflush(stdout);
-
     fd_set readfds;
     char input[128];
     char reply[256];
-
+    
     while (1) {
         FD_ZERO(&readfds);
         FD_SET(STDIN_FILENO, &readfds);
@@ -216,11 +200,9 @@ void run_frontend(int pipe_fe_to_disp[2], int pipe_disp_to_fe[2], pid_t dispatch
                 fflush(stdout);
             }
         }
-
         if (FD_ISSET(STDIN_FILENO, &readfds)) {
             if (fgets(input, sizeof(input), stdin) == NULL) break;
             input[strcspn(input, "\n")] = 0;
-
             if (strcmp(input, "add") == 0 || strncmp(input, "search ", 7) == 0 || strcmp(input, "exit") == 0) {
                 if (strcmp(input, "add") == 0) write(pipe_fe_to_disp[1], "ADD", 4);
                 else if (strcmp(input, "exit") == 0) write(pipe_fe_to_disp[1], "EXIT", 5);
@@ -229,7 +211,6 @@ void run_frontend(int pipe_fe_to_disp[2], int pipe_disp_to_fe[2], pid_t dispatch
                     sprintf(cmd, "SEARCH %c", input[7]);
                     write(pipe_fe_to_disp[1], cmd, strlen(cmd) + 1);
                 }
-
                 if (strcmp(input, "exit") == 0) break;
             }
             else if (strcmp(input, "status") == 0) {
@@ -246,18 +227,15 @@ int main(int argc, char *argv[]) {
     if (argc != 2) {
         exit(1);
     }
-
     int pipe_fe_to_disp[2], pipe_disp_to_fe[2];
     pipe(pipe_fe_to_disp);
     pipe(pipe_disp_to_fe);
-
     pid_t dispatcher_pid = fork();
     if (dispatcher_pid == 0) {
         run_dispatcher(argv[1], pipe_fe_to_disp, pipe_disp_to_fe, getppid());
         exit(0);
     }
     run_frontend(pipe_fe_to_disp, pipe_disp_to_fe, dispatcher_pid);
-
     waitpid(dispatcher_pid, NULL, 0);
     return 0;
 }
